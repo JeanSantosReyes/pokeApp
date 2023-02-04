ef181c4424f3c489a7656986699fe246455e291e
Jean Santos <santosjeandx@gmail.com>
2023-02-04 08:58:42 -0500

    U

--- src/components/PokeCell.jsx
+++ src/components/PokeCell.jsx
@@ -1,11 +1,10 @@
 // import sprites from '../sprites.png';
-import sprites from '../sprites.png'
 import './styles/Cells.css';
 import '../../node_modules/nes.css/css/nes.css';
 
 export const PokeCell = ({ pokeClass, handleOnClick }) => {
     const { id, backgroundPosition } = pokeClass;
-    const style = { backgroundImage: `url(${sprites})`, backgroundPosition };
+    const style = { backgroundImage: `url(../../sprites.png)`, backgroundPosition };
     return (
         <>
             <button
